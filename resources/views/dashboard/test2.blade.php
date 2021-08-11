4372b2ba7bfe0554b3548e58dd8ec2d154016068
Teng Wei Kang <weikangteng@gmail.com>
2021-08-11 13:00:39 +0800

    Add datatables plugins

---
+++ resources/views/dashboard/test2.blade.php
@@ -0,0 +1,57 @@
+@extends('dashboard.layouts.template')
+
+@section('title')
+    test title
+@endsection
+
+@section('content')
+<div class="row mt-3">
+    <div class="col-lg-12">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Basic Table</h5>
+          <div class="table-responsive">
+            <table id="table" class="">
+                <thead>
+                    <tr>
+                        <th class="">Name</th>
+                        <th>Age</th>
+                        <th>Office</th>
+                        <th>Address</th>
+                        <th>Start Date</th>
+                        <th class="">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Lorem ipsum dolor sit amet.</td>
+                        <td>Lorem ipsum dolor sit amet.</td>
+                        <td>Lorem ipsum dolor sit amet.</td>
+                        <td>Lorem ipsum dolor sit amet.</td>
+                        <td>Lorem ipsum dolor sit amet.</td>
+                        <td>Lorem ipsum dolor sit amet.</td>
+                    </tr>
+                    <tr>
+                        <td>Lorem ipsum dolor sit amet.</td>
+                        <td>Lorem ipsum dolor sit amet.</td>
+                        <td>Lorem ipsum dolor sit amet.</td>
+                        <td>Lorem ipsum dolor sit amet.</td>
+                        <td>Lorem ipsum dolor sit amet.</td>
+                        <td>Lorem ipsum dolor sit amet.</td>
+                    </tr>
+                </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div><!--End Row-->
+@endsection
+
+@push('script')
+    <script>
+    $(document).ready(function () {
+        $("#table").DataTable();
+    });
+    </script>
+@endpush
