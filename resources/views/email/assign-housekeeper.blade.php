c7efcce1e869d8c8ef6222a6b7d7039eededad2b
Teng Wei Kang <weikangteng@gmail.com>
2021-09-06 22:17:34 +0800

    modify email content of assigning housekeeper email

--- resources/views/email/assign-housekeeper.blade.php
+++ resources/views/email/assign-housekeeper.blade.php
@@ -1,11 +1,13 @@
 @component('mail::message')
 Dear {{ $housekeeper->username }},
 
-You have been assign to housekeep the Room <a href="{{ route("dashboard.room.view", ["room" => $room]) }}">{{ $room->room_id }}</a>.
+You have been assign to housekeep the Room <a href="{{ route("login", ["redirect" => route("dashboard.room.view", ["room" => $room])]) }}">{{ $room->room_id }}</a>.
 
 @component('mail::button', ["url" => route("login", ["redirect" => route("dashboard.room.view", ["room" => $room])])])
 Update Clean
 @endcomponent
+
+Please update the status after cleaning the room.
 
 Thanks & Regards,<br>
 {{ config('app.name') }}
